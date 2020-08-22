396ddb330f7942bf07f23f2397a3085e38bcff95
manender <manender369@gmail.com>
2020-08-22 13:33:35 -0400

    added event page

--- src/app/frontend/frontend-pages/frontend-event-details/frontend-event-details/frontend-event-details.component.ts
+++ src/app/frontend/frontend-pages/frontend-event-details/frontend-event-details/frontend-event-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TheEvent } from 'src/app/models/event/event.model';
 
 @Component({
   selector: 'app-frontend-event-details',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrontendEventDetailsComponent implements OnInit {
 
-  constructor() { }
+  images: string[] = [];
+  theEvent: TheEvent;
+
+  constructor( ) { }
 
   ngOnInit(): void {
+    this.getEventData();
   }
 
+
+  getEventData() {
+    this.theEvent = {
+      id: '',
+      name: 'Event Name',
+      description: `Ut euismod ultricies sollicitudin. Curabitur sed dapibus nulla. Nulla eget iaculis lectus. Mauris ac maximus neque. Nam in mauris quis libero sodales eleifend. Morbi varius, nulla sit amet rutrum elementum, est elit finibus tellus, ut tristique elit risus at metus.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in pulvinar neque. Nulla finibus lobortis pulvinar. Donec a consectetur nulla. Nulla posuere sapien vitae lectus suscipit, et pulvinar nisi tincidunt. Aliquam erat volutpat. Curabitur convallis fringilla diam sed aliquam. Sed tempor iaculis massa faucibus feugiat. In fermentum facilisis massa, a consequat purus viverra.`,
+      imageUrl: 'https://picsum.photos/id/1027/1200/300',
+      date: new Date(),
+      active: true,
+      featured: true,
+      order: 100,
+      created: new Date(),
+      modified: new Date()
+    };
+  }
 }
