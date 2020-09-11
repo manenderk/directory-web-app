556476971f75ff80366d891926dc7b15d9b0f5ff
manender <manender369@gmail.com>
2020-09-11 14:41:52 -0400

    category backend updates

--- src/app/admin/admin-modules/category/category-list/category-list.component.ts
+++ src/app/admin/admin-modules/category/category-list/category-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgxBasicTableColMeta, NgxBasicTableMeta } from '@manenderk/ngx-basic-table';
 import { Category } from 'src/app/models/category/category.model';
 import { CategoryService } from 'src/app/services/category/category.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-category-list',
@@ -9,10 +12,41 @@ import { CategoryService } from 'src/app/services/category/category.service';
 })
 export class CategoryListComponent implements OnInit {
 
+  tabMeta: NgxBasicTableMeta = {
+    classes: {
+      table: 'table'
+    }
+  };
+
+  colMeta: NgxBasicTableColMeta[] = [
+    {
+      key: 'name',
+      displayName: 'Name',
+      dataType: 'string',
+      link: true
+    },
+    {
+      key: 'active',
+      displayName: 'Active',
+      dataType: 'string'
+    },
+    {
+      key: 'featured',
+      displayName: 'Featured',
+      dataType: 'string',
+    },
+    {
+      key: 'thumbnail',
+      displayName: 'Thumbnail',
+      dataType: 'image'
+    }
+  ];
+
   categories: Category[];
 
   constructor(
-    private catService: CategoryService
+    private catService: CategoryService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -22,6 +56,10 @@ export class CategoryListComponent implements OnInit {
   async getCategories() {
     this.categories = await this.catService.getCategories().toPromise();
     console.log(this.categories);
+  }
+
+  categoryClicked(cat: Category) {
+    this.router.navigate(['/' + environment.adminRoutePrefix + '/category/category-edit/' + cat.id]);
   }
 
 }
