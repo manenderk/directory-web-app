1cc9a9f3cfcbcd396adc6dcd3606bfdbbb84d36e
Manender <manender369@gmail.com>
2020-10-06 12:40:06 -0400

    changed prod urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,9 @@ export const environment = {
   appName: 'BizBox',
   appLogo: 'assets/frontend/images/logo-bb.JPG',
   adminRoutePrefix: 'dir-adm',
-  apiHost: 'http://localhost:3000/',
-  fileHost: 'http://localhost/'
+  apiHost: 'https://manender.com/directory-api/',
+  fileHost: 'https://manender.com/directory-api/'
 };
+
+
+// BUILD COMMAND : ng build --prod --base-href=/directory/ --deploy-url=/directory/
